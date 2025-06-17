8cd63767a45c2f57bdd5bdc3f6c4cee9d3f51146
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-06-17 18:47:09 -0300

    card

--- src/components/sections/home.category.tsx
+++ src/components/sections/home.category.tsx
@@ -1,30 +1,25 @@
-
-
 import { Avatar, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 
-
 function HomeCategorySection() {
-
-
-
-    return <Grid container spacing={2}>
-        <Grid size={12}>
-            <Typography variant="h5"> Categorias en destaque</Typography>
+  return (
+    <Grid container spacing={2}>
+      <Grid size={12}>
+        <Typography variant="h5"> Categorias en destaque</Typography>
+      </Grid>
+      {Array.from(Array(6)).map((_, key) => (
+        <Grid key={key} size={{ xs: 12, sm: 6, md: 3, lg: 2 }}>
+          <Card>
+            <CardContent>
+              <Stack direction="column" alignItems="center" gap={2}>
+                <Avatar src="https://picsum.photos/200/300" />
+                <Typography variant="overline">Man clothes</Typography>
+              </Stack>
+            </CardContent>
+          </Card>
         </Grid>
-        {Array.from(Array(6)).map((_, key) => (
-            <Grid size={{ xs: 12, sm: 6, md: 3, lg: 2 }} >
-                <Card>
-                    <CardContent>
-                        <Stack direction='column' alignItems='center' gap={2}>
-                            <Avatar src="https://picsum.photos/200/300" />
-                            <Typography variant="overline">
-                                Man clothes
-                            </Typography>
-                        </Stack>
-                    </CardContent>
-                </Card>
-            </Grid>))}
+      ))}
     </Grid>
+  );
 }
 
 export default HomeCategorySection;
--- src/modules/async-components/productos/index.tsx
+++ src/modules/async-components/productos/index.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { productosApiService } from "services/api/producto/producto";
 
 async function getData() {
@@ -11,25 +11,14 @@ async function getData() {
 
 async function Productos() {
   const data = await getData();
-  console.log(data);
+
   return (
     <Grid container spacing={2}>
-      {data.map((i, key) => (
+      {data.map((e, key) => (
         <Grid size={{ xs: 6, sm: 4, md: 3 }} key={key}>
-          <Card sx={{ maxHeight: 345, height: "100%" }}>
-            <CardMedia component="img" height="140" image={i.images[0]?.url || "no-image.jpg"} alt={i.name} sx={{ objectFit: "contain", padding: 2 }} />
-            <CardContent>
-              <Typography variant="caption" color="text.secondary">
-                {i.codigo}
-              </Typography>
-              <Typography variant="caption" display="block">
-                {i.nombre}
-              </Typography>
-              <Typography variant="caption" display="block" fontWeight="bold">
-                {i.precio_normal}
-              </Typography>
-            </CardContent>
-          </Card>
+          <Box borderRadius={2} boxShadow={2} p={2}>
+            <Typography variant="body2">{e.nombre}</Typography>
+          </Box>
         </Grid>
       ))}
     </Grid>
